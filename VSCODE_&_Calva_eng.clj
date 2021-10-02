69e527420f9001d1524b8395a365145e3dcf618f
pinkbanana <pinkbananacoding@gmail.com>
2021-10-02 17:01:25 +0200

    Initial commit

---
+++ VSCODE_&_Calva_eng.clj
@@ -0,0 +1,29 @@
+;-VS-code-----------------------------------------------------------------------------------------------
+;//CTRL+SHIFT(Arrow key up)+P//
+;opens search window
+;
+;-Calva-----------------------------------------------------------------------------------------------
+;//ALT+ENTER//
+;evaluate top-level form
+;
+;//CTRL+ENTER//
+;evalate the left code from your cursor
+;
+;//CTRL+CLICK On A FUNCTION //
+;guides you to the definition of the function 
+,for example: directs you to the code part of a java code of another file that defines the function
+;
+;//CTRL+ALT+C CTRL+ALT+D//
+;stopps infinite loops
+;
+;//ALT+BACKSPACE//
+;delete a bracket 
+;
+;//CTRL+C+ENTER//
+;load file
+;
+;//CTRL+ALT+C CTRL+ALT+D//
+;connect repl with current form
+;
+;//CTRL+D//
+;inside of a powershell= Leave REPL
---
+++ VSCode_&_Calva_de.clj
@@ -0,0 +1,29 @@
+;-VS-code-----------------------------------------------------------------------------------------------
+;//CTRL+SHIFT(Pfeiltaste oben)+P//
+;öffnet Suchfenster
+;
+;-Calva-----------------------------------------------------------------------------------------------
+;//ALT+ENTER//
+;Evaluiere den code als Top-Level-Form 
+;
+;//CTRL+ENTER//
+;evalue den linken Code vom Cursor aus
+;
+;//CTRL+CLICK AUF EINE FUNKTION //
+;führt dich zur Definition der Funktion 
+;z.B.: leitet dich zum Code-Teil eines Java-Codes einer anderen Datei, die die Funktion definiert
+;
+;//CTRL+ALT+C CTRL+ALT+D//
+;stoppt Endlosschleife
+;
+;//ALT+BACKSPACE//
+;lösche eine Klammer 
+;
+;//CTRL+C+ENTER//
+;Datei laden
+;
+;//CTRL+ALT+C CTRL+ALT+D//
+;repl mit aktuellem Formular verbinden
+;
+;//CTRL+D//
+;innerhalb einer Powershell: REPL verlassen
